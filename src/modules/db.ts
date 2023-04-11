1e06d7ec2bf089eaf6a0bbf9f54a14199c314e88
Makarenko Aleksandr <rolliks@gmail.com>
2023-04-11 15:13:49 +0500

    dotenv types, db pool

--- src/modules/db.ts
+++ src/modules/db.ts
@@ -1,9 +1,10 @@
 import { createNativeClient, getDefaultLibraryFilename } from 'node-firebird-driver-native';
-import { /*ResultSet, FetchOptions,*/ ConnectOptions } from 'node-firebird-driver';
+import { /*ResultSet, FetchOptions,*/ Attachment, ConnectOptions } from 'node-firebird-driver';
+import { createPool } from 'generic-pool';
+
 import dotenv from 'dotenv';
 dotenv.config();
-//:import { process } from '../server.js';
-//const fetchOne: FetchOptions = { fetchSize: 1 };
+
 const DB_HOST = process.env.DB_HOST;
 const DB_PORT = process.env.DB_PORT;
 const DB_NAME = process.env.DB_NAME;
@@ -13,16 +14,40 @@ const connectionString = `${DB_HOST}/${DB_PORT}:${DB_NAME}`;
 console.log(connectionString);
 const loginString: ConnectOptions = { username: `${DB_USER}`, password: `${DB_PASSWORD}` };
 
-const client = createNativeClient(getDefaultLibraryFilename());
-const attachment = await client.connect(connectionString, loginString);
-console.log(`подключение к базе данных = ${attachment.isValid}`);
+/* фабрика коннектов к базе */
+const dbFactory = {
+  create: function () {
+    const client = createNativeClient(getDefaultLibraryFilename());
+    const attachment = client.connect(connectionString, loginString);
+    console.log(`подключение к базе данных`);
+    return attachment;
+  },
+  destroy: function (connection: Attachment) {
+    console.log(`закрыть базу данных`);
+    return connection.disconnect();
+  },
+};
 
-export async function QueryOpen(sql: string, prm: undefined[]) {
+const opts = {
+  max: 100, // maximum size of the pool
+  min: 2, // minimum size of the pool
+};
+
+const dbPool = createPool(dbFactory, opts);
+
+export const handleExit = () => {
+  console.log(`close pool`);
+  dbPool.drain().then(() => dbPool.clear());
+};
+
+export async function QueryOpen(sql: string, prm: undefined[]): Promise<object[]> {
+  const attachment = await dbPool.acquire();
   console.log(`SQL = ${sql}`);
   console.log(`PRM = ${prm}`);
   const transaction = await attachment.startTransaction();
   const stat = await attachment.prepare(transaction, sql, undefined);
   const recSet = await stat.executeQuery(transaction, prm, undefined);
   const res = await recSet.fetchAsObject();
+  dbPool.release(attachment);
   return res;
 }
--- src/server.ts
+++ src/server.ts
@@ -1,22 +1,16 @@
 import app from './app.js';
 import * as dotenv from 'dotenv';
-
-declare let process: {
-  env: {
-    PORT: number;
-    SERVER: string;
-    MODE_ENV: string;
-    DB_PORT: number;
-    DB_HOST: string;
-    DB_NAME: string;
-    DB_USER: string;
-    DB_PASSWORD: string;
-  };
-};
+import { handleExit } from './modules/db.js';
 
 dotenv.config();
 
-//const PORT = Number(process.env.PORT) || 3333;
-const PORT = process.env.PORT || 3333;
+['SIGINT', 'SIGTERM', 'SIGQUIT'].forEach((signal) =>
+  process.on(signal, () => {
+    handleExit();
+    process.exit();
+  }),
+);
+
+const PORT = Number(process.env.PORT) || 3333;
 const SERVER = process.env.SERVER || '127.0.0.1';
 app.listen(PORT, SERVER, () => console.log(`start server at IP ADDRESS:${SERVER} PORT:${PORT}`));
